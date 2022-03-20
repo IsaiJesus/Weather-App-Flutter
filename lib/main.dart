14636113c4fdd4caa75beffe6e331d68dbc437ff
Isai Jesus <isaijesus02@gmail.com>
2022-03-19 19:16:01 -0600

    Adding API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/model/weather_model.dart';
+import 'package:weather_app/services/weather_api_client.dart';
 import 'package:weather_app/views/additional_information.dart';
 import 'package:weather_app/views/current_weather.dart';
 
@@ -27,6 +29,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  WeatherApiClient client = WeatherApiClient();
+  Weather? data;
+
+  Future<void> getData() async{
+    data = await client.getCurrentWeather("Mexico City");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,28 +52,43 @@ class _HomePageState extends State<HomePage> {
           color: Colors.black,
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          currentWeather(Icons.wb_sunny_rounded, "26.3", "Georgia"),
-          SizedBox(
-            height: 60.0,
-          ),
-          Text(
-            "Additional Information", 
-            style: TextStyle(
-              fontSize: 24.0, 
-              color: Color(0xdd212121), 
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Divider(),
-          SizedBox(
-            height: 20.0,
-          ),
-          additionalInformation("24", "2", "1014", "24.6"),
-        ],
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot){
+          if(snapshot.connectionState == ConnectionState.done){
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                currentWeather(Icons.wb_sunny_rounded, "${data!.temp}°", "${data!.cityName}"),
+                SizedBox(
+                  height: 60.0,
+                ),
+                Text(
+                  "Additional Information", 
+                  style: TextStyle(
+                    fontSize: 24.0, 
+                    color: Color(0xdd212121), 
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Divider(),
+                SizedBox(
+                  height: 20.0,
+                ),
+                additionalInformation(
+                  "${data!.wind}", 
+                  "${data!.humidity}", 
+                  "${data!.pressure}", 
+                  "${data!.feels_like}"
+                  ),
+              ],
+            );
+          }else if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          return Container();
+        },
       ),
-    );
+      );
   }
 }
